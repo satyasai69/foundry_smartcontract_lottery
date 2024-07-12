96fa6207c869e3dd98e609586980ff74e76305eb
satyasai69 <ch.satya.sai133@gmail.com>
2024-07-12 14:07:30 +0530

    Raffle main functions

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,7 +1,27 @@
-// SPDX-License-Identifer: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
 
+/**
+ * @title A simple raffle contract
+ * @author me
+ * @notice This contract to creact a  simple raffle
+ * @dev ChainlinkVRFv2.5
+ */
 contract Raffle {
+    uint256 private immutable i_enterancefee;
 
+    constructor(uint256 enterancefee) {
+        i_enterancefee = enterancefee;
+    }
+
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+
+    /** Getter function to read enterancefee */
+
+    function getEnterancefee() public view returns (uint256) {
+        return i_enterancefee;
+    }
 }
