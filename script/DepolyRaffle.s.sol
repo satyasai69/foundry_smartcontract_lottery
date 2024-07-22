979d26481c5e7f9f15d2409cfdd48ffe36c06321
satyasai69 <ch.satya.sai133@gmail.com>
2024-07-22 12:27:06 +0530

    Depoly contract

--- script/DepolyRaffle.s.sol
+++ script/DepolyRaffle.s.sol
@@ -3,11 +3,26 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 import {Raffle} from "src/Raffle.sol";
 
 contract DepolyRaffle is Script {
     function run() public {}
 
-    function depolyContract() public returns (Raffle, HelperConfig) {}
+    function depolyContract() public returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.enterancefee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+        return (raffle, helperConfig);
+    }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -46,6 +46,10 @@ contract HelperConfig is CodeConstants, Script {
         }
     }
 
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         return
             NetworkConfig({
